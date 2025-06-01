aa1caf14ad6eddd029205a38831f871207a012a5
Le Viet <levietnee@gmail.com>
2025-06-01 22:40:38 +0700

    fix: update permissions for invite link to provide full access for the bot

--- utils/inviteLink.js
+++ utils/inviteLink.js
@@ -2,11 +2,8 @@ require('dotenv').config();
 
 // Tạo invite link với permissions cần thiết
 const botId = process.env.CLIENT_ID;
-// Permissions cần thiết cho bot nhạc:
-// - Xem kênh, gửi tin nhắn, nhúng links
-// - Kết nối voice, nói, phát hiện giọng nói
-// - applications.commands cho slash commands
-const permissions = '36768832';
+// Toàn quyền cho bot (ADMINISTRATOR + applications.commands)
+const permissions = '274877975552';
 
 const inviteLink = `https://discord.com/api/oauth2/authorize?client_id=${botId}&permissions=${permissions}&scope=bot%20applications.commands`;
 
